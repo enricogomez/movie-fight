ae7830e1083d691c88151aaddc379a83f28d2f67
unknown <enricocamilogomezjr@gmail.com>
2022-02-12 23:23:20 +0800

    second commit

--- autocomplete.js
+++ autocomplete.js
@@ -13,16 +13,9 @@ const createAutoComplete = ({ root,renderOption,onOptionSelect,inputValue,fetchD
   const dropdown = root.querySelector('.dropdown');
   const resultsWrapper = root.querySelector('.results');
   
-  input.addEventListener('input', debounce(onInput));
-  document.addEventListener('click', event => {
-    if (!root.contains(event.target)) {
-      dropdown.classList.remove('is-active');
-    }
-  });
-
   const onInput = async (event) => {
     const items = await fetchData(event.target.value);
-  
+
     if (!items.length) {
       dropdown.classList.remove('is-active');
       return;
@@ -30,8 +23,9 @@ const createAutoComplete = ({ root,renderOption,onOptionSelect,inputValue,fetchD
     resultsWrapper.innerHTML = '';
     dropdown.classList.add('is-active');
     
-    for (let item of items){
+    for (let item of items) {
       const option = document.createElement('a');
+      
       option.classList.add('dropdown-item');
       option.innerHTML = renderOption(item);
       option.addEventListener('click', () => {
@@ -42,5 +36,12 @@ const createAutoComplete = ({ root,renderOption,onOptionSelect,inputValue,fetchD
       resultsWrapper.appendChild(option);
     }
   };
+
+  input.addEventListener('input', debounce(onInput));
+  document.addEventListener('click', event => {
+    if (!root.contains(event.target)) {
+      dropdown.classList.remove('is-active');
+    }
+  });
 };
 
